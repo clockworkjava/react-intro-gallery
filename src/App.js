d0742112fdf0f6d1c9b9ae17ddc35e6a4312fd27
Pawel.Cwik <pawel.cwik@sabre.com>
2020-02-25 15:51:57 +0100

    Forms

--- src/App.js
+++ src/App.js
@@ -22,28 +22,39 @@ export const Showcase = ({ url }) => {
 export const App = () => {
   const [showcaseURL, setShowcaseURL] = useState("");
 
+  const defaultImages = [
+    "https://images.unsplash.com/photo-1508138221679-760a23a2285b",
+    "https://images.unsplash.com/photo-1474487548417-781cb71495f3",
+    "https://images.unsplash.com/photo-1580109672851-b85640868813",
+    "https://images.unsplash.com/photo-1580046939256-c377c5b099f1",
+    "https://images.unsplash.com/photo-1576801488695-2e4d7a14b8b5"
+  ];
+
+  const [images, setImages] = useState(defaultImages);
+  const [newImageUrl, setNewImageUrl] = useState("");
+
+  const imgComponents = images.map(url => {
+    return <Image url={url} key={url} setShowcaseURL={setShowcaseURL} />;
+  });
+
+  const addImage = event => {
+    event.preventDefault();
+    event.stopPropagation();
+    setImages([...images, newImageUrl]);
+  };
+
   return (
     <div>
-      <Image
-        url="https://images.unsplash.com/photo-1508138221679-760a23a2285b"
-        setShowcaseURL={setShowcaseURL}
-      />
-      <Image
-        url="https://images.unsplash.com/photo-1474487548417-781cb71495f3"
-        setShowcaseURL={setShowcaseURL}
-      />
-      <Image
-        url="https://images.unsplash.com/photo-1580109672851-b85640868813"
-        setShowcaseURL={setShowcaseURL}
-      />
-      <Image
-        url="https://images.unsplash.com/photo-1580046939256-c377c5b099f1"
-        setShowcaseURL={setShowcaseURL}
-      />
-      <Image
-        url="https://images.unsplash.com/photo-1576801488695-2e4d7a14b8b5"
-        setShowcaseURL={setShowcaseURL}
-      />
+      <form>
+        <input
+          type="text"
+          placeholder="URL obrazu..."
+          value={newImageUrl}
+          onChange={e => setNewImageUrl(e.target.value)}
+        ></input>
+        <button onClick={addImage}>Dodaj</button>
+      </form>
+      {imgComponents}
       <Showcase url={showcaseURL} />
     </div>
   );
